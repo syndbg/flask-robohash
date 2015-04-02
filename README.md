1de9799e11768e1d5ad04a978b724682049e6282
syndbg <syndbe@gmail.com>
2015-04-03 02:02:25 +0300

    Added working version

--- README.md
+++ README.md
@@ -67,11 +67,16 @@ def profile():
 
 ## Robohash parameters
 
-Robohash.org doesn't reveal all of its parameters clearly, but I managed to gather them from the source code.
+Robohash.org doesn't reveal all of its parameters clearly, but I managed to gather them from the source code and add a bit more to my liking.
 
-* *INT* x - Image horizontal size
-* *INT* y - Image vertical size
-* *STR* format - 'bmp', 'jpg' or 'png'. The output of the image.
-* *STR* bgset - '1', '2' or '3' or 'any'. The available background sets.
-* *STR* size - '200x200'. Constructed by params `x` and `y`. Recommended that you use `x` and `y` instead of `size`.
-
+* *INT* x (default 300) - Image horizontal size
+* *INT* y (default 300) - Image vertical size
+* *STR* size (default 300x300) - Constructed by params `x` and `y`. Recommended that you use `x` and `y` instead of `size`.
+* *STR* format (optional) - 'bmp', 'jpg' or 'png'. The output of the image.
+* *STR* bgset (optional) - 'bg1', 'bg2', 'bg3', '1', '2', '3' or 'any'. The available background sets.
+* *STR* set (optional)  - 'robots', 'zombies', 'heads', 1, 2 or 3. What random creature type to generate.
+* *STR* color (optional)  - The creature's color. Used to filter robots/zombies/heads by color.
+* *STR* force_hash (optional and default True) - To prevent exposing user data, hash the given `text` with `md5` by default.
+* *STR* hash_algorithm (optional and default md5) - Hashing algorithm to use if `force_hash`. Supports only those in `hashlib.algorithms_available`.
+* *STR* use_gravatar (optional and default False) - If passed `text` is a recognized email in gravatar, built link will forward to a gravatar IMG url.
+* *STR* gravatar_hashed (optional and default False) - If passed `text` is a recognized md5 email hash in gravatar, built link will forward to a gravatar IMG url.
---
+++ flask_robohash/__init__.py
@@ -0,0 +1,80 @@
+from hashlib import algorithms_available
+import hashlib
+
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+
+
+class Robohash(object):
+    DEFAULT_SIZE_X = 300
+    DEFAULT_SIZE_Y = 300
+    DEFAULT_HASH_ALGORITHM = 'md5'
+    ALLOWED_FORMATS = ('png', 'jpg', 'bmp')
+    ALLOWED_BGSETS = ('any', '1', '2', '3', 'bg1', 'bg2', 'bg3')
+    ALLOWED_CREATURE_SETS = {'robots': 1, 'zombies': 2, 'heads': 3}
+
+    def __init__(self, **kwargs):
+        self.size_x = kwargs.get('x', self.DEFAULT_SIZE_X)
+        self.size_y = kwargs.get('y', self.DEFAULT_SIZE_Y)
+        self.size = kwargs.get('size')
+        self.format = kwargs.get('format')
+        self.bgset = kwargs.get('bgset')
+        self.creature_set = kwargs.get('set')
+        self.color = kwargs.get('color')
+        self.force_hash = kwargs.get('force_hash', True)
+        self.hash_algorithm = kwargs.get('hash_algorithm', 'md5')
+        self.use_gravatar = kwargs.get('use_gravatar', False)
+        self.gravatar_hashed = kwargs.get('use_gravatar', False)
+
+        self.app = kwargs.get('app')
+        if self.app:
+            self.init_app(self.app, **kwargs)
+
+    def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.jinja_env.filters.setdefault('robohash', self)
+        app.extensions['robohash'] = self
+
+    def __call__(self, text, **kwargs):
+        size = '{0}x{1}'.format(kwargs.get('x', self.size_x), kwargs.get('y', self.size_y)) or kwargs.get('size', self.size)
+        format = kwargs.get('format', self.format)
+        bgset = kwargs.get('bgset', self.bgset)
+        creature_set = kwargs.get('set', self.creature_set)
+        color = kwargs.get('color', self.color)
+        force_hash = kwargs.get('force_hash', self.force_hash)
+        hash_algorithm = kwargs.get('hash_algorithm', self.hash_algorithm)
+        use_gravatar = kwargs.get('use_gravatar', self.use_gravatar)
+        gravatar_hashed = kwargs.get('gravatar_hashed', self.gravatar_hashed)
+
+        url = 'https://robohash.org/'
+        if use_gravatar or gravatar_hashed:
+            url += '{0}?'.format(text)
+            if use_gravatar:
+                url += '&gravatar=yes'
+            else:
+                url += '&gravatar=hashed'
+            return url
+        elif force_hash and hash_algorithm in algorithms_available:
+            hash_func = getattr(hashlib, hash_algorithm)
+            text = hash_func(text).hexdigest()
+
+        url += '{0}?'.format(text)
+        if format in self.ALLOWED_FORMATS:
+            param = 'format={0}'.format(format)
+            url += param if url.endswith('?') else '?{0}'.format(param)
+        if size:
+            param = 'size={0}'.format(size)
+            url += param if url.endswith('?') else '?{0}'.format(param)
+        if bgset in self.ALLOWED_BGSETS:
+            param = 'bgset={0}'.format('bg{0}'.format(bgset) if bgset.isdigit() else bgset)
+            url += param if url.endswith('?') else '?{0}'.format(param)
+        if color:
+            param = 'color={0}'.format(color)
+            url += param if url.endswith('?') else '?{0}'.format(param)
+        if creature_set in self.ALLOWED_CREATURE_SETS.items():
+            param = 'set={0}'.format(self.ALLOWED_CREATURE_SETS[creature_set] if creature_set in self.ALLOWED_CREATURE_SETS else creature_set)
+            url += param if url.endswith('?') else '?{0}'.format(param)
+        return url
